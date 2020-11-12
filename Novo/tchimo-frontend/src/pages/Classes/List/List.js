e5c4bdd34db4f20a487d0adf2bf1b32039076242
Wesley-M <wesley.santos@ccc.ufcg.edu.br>
2020-11-12 10:41:40 -0300

    Creating template of the class list screen

--- Novo/tchimo-frontend/src/pages/Classes/List/List.js
+++ Novo/tchimo-frontend/src/pages/Classes/List/List.js
@@ -2,11 +2,50 @@ import React from 'react'
 import TchimoHeader from '../../../components/TchimoHeader/TchimoHeader'
 import TabbedMenu from '../../../components/TabbedMenu/TabbedMenu'
 
+import '../../../App.css'
+import styles from './List.module.css'
+
 function List() {
     return (
         <>
             <TchimoHeader />
-            <p>Salas</p>
+            <p className="session">Salas</p>
+
+            <div className={styles.classesContainer}>
+                <div className={styles.classContainer}>
+                    <span className={styles.className}>Engenharia de Software</span>
+                    <span className={styles.classCode}>CÓDIGO: 989347</span>
+                    <span className={styles.classInfo}>22 membros - 2 grupos</span>
+                    <div className={styles.classOptions}>
+                        <span className={styles.classSolicitation}>
+                            Sair da Turma
+                        </span>
+                    </div>
+                </div>
+
+                <div className={styles.classContainer}>
+                    <span className={styles.className}>Engenharia de Software</span>
+                    <span className={styles.classCode}>CÓDIGO: 989347</span>
+                    <span className={styles.classInfo}>22 membros - 2 grupos</span>
+                    <div className={styles.classOptions}>
+                        <span className={styles.classSolicitation}>
+                            Sair da Turma
+                        </span>
+                    </div>
+                </div>
+
+                <div className={styles.classContainer}>
+                    <span className={styles.className}>Engenharia de Software</span>
+                    <span className={styles.classCode}>CÓDIGO: 989347</span>
+                    <span className={styles.classInfo}>22 membros - 2 grupos</span>
+                    <div className={styles.classOptions}>
+                        <span className={styles.classSolicitation}>
+                            Sair da Turma
+                        </span>
+                    </div>
+                </div>
+            </div>
+
             <TabbedMenu />
         </>
     )
