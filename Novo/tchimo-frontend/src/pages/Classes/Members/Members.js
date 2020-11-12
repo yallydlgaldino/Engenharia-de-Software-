97ccb5dd946bbe2e8b0d8d10466f10d9822f1fff
Wesley-M <wesley.santos@ccc.ufcg.edu.br>
2020-11-11 23:06:38 -0300

    Adding the style for the members list screen

--- Novo/tchimo-frontend/src/pages/Classes/Members/Members.js
+++ Novo/tchimo-frontend/src/pages/Classes/Members/Members.js
@@ -8,8 +8,10 @@ import PeopleIcon from '@material-ui/icons/People';
 import SupervisedUserCircleIcon from '@material-ui/icons/SupervisedUserCircle';
 import ListIcon from '@material-ui/icons/List';
 import NotificationsActiveIcon from '@material-ui/icons/NotificationsActive';
+import EmailIcon from '@material-ui/icons/Email';
 
 import '.././../../App.css'
+import styles from './Members.module.css'
 
 function Members(props) {
 
@@ -21,7 +23,23 @@ function Members(props) {
 
           <p className="session">Membros da Turma</p>
           
-          
+          <div className={styles.membersContainer}>
+            <div className={styles.memberContainer}>
+              <span className={styles.memberName}>Wesley Santos</span>
+              <button><EmailIcon /></button>
+            </div>
+
+            <div className={`${styles.memberContainer} ${styles.active}`}>
+              <span className={styles.memberName}>Wesley Santos</span>
+              <button><EmailIcon /></button>
+            </div>
+
+            <div className={`${styles.memberContainer} ${styles.invite}`}>
+              <span className={styles.memberName}>Wesley Santos</span>
+              <button><EmailIcon /></button>
+            </div>
+          </div>
+
           <TabbedMenu 
             customData={{ code }}
             customLinks={
